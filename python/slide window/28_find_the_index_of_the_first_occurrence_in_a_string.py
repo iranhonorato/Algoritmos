032e2fd724eaf6010c42504faf0e02e8afe2feda
iranhonorato <antonioihsj@al.insper.edu.br>
2026-09-11 10:56:02 -0300

    feat: implementando solução de slide window

---
+++ python/slide window/28_find_the_index_of_the_first_occurrence_in_a_string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        l = 0 
+        r = len(needle)
+
+        ans = 0
+
+        while r <= len(haystack):
+            if haystack[l:r] == needle:
+                return l 
+
+            r +=1
+            l += 1 
+
+        return -1
