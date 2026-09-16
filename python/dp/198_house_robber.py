c2b04f66de4d41ef10f68403a514549d631e9a87
iranhonorato <iranilsonhonorato88@gmail.com>
2026-09-16 15:31:42 -0300

    fix: corrigindo arquivo

--- python/dp/198_house_robber.py
+++ python/dp/198_house_robber.py
@@ -73,9 +73,3 @@ class Solution:
             return memo[i]
 
         return backtracking(0)
-
-
-print(Solution().rob([0] * 100))  # Output: 0 (Runs in < 1 millisecond)
-
-print(Solution().rob([1, 2, 3, 1]))  # Output: 4
-print(Solution().rob([2, 7, 9, 3, 1]))  # Output: 12
