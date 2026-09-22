107ec7c42c2f7113da3536940dadc60bad5d68bd
iranhonorato <iranilsonhonorato88@gmail.com>
2026-09-22 15:50:20 -0300

    feat: mais uma de dp

--- python/backtracking/2305.fair_distribution_of_cookies.py
+++ python/backtracking/2305.fair_distribution_of_cookies.py
@@ -1,5 +1,34 @@
 from typing import List
 
 class Solution:
-    def distributeCookies(self, cookies: List[int], k: int) -> int:
-        pass
+    def distributeCookies(self, cookies: list[int], k: int) -> int:
+        children = [0]*k
+        ans = [sum(cookies)]
+
+        def backtracking(idx):
+            if idx == len(cookies):
+                ans[0] = min(ans[0], max(children))
+                return
+
+            seen = set()
+
+            for idx_child in range(k):
+                if children[idx_child] in seen:
+                    continue 
+                seen.add(children[idx_child])
+                print(seen)
+
+                children[idx_child] += cookies[idx]
+
+                backtracking(idx+1)
+
+                children[idx_child] -= cookies[idx]
+            return
+
+        backtracking(0)
+        return ans[0]
+
+
+
+obj = Solution()
+print(obj.distributeCookies(cookies = [6,1,3,2,2,4,1,2], k = 3))
